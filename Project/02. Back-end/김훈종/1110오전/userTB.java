d8115fde593f0861006b6cbfc3f62e2d34c5f668
김훈종 <139337239+parvenuHJ@users.noreply.github.com>
2023-11-10 13:52:53 +0900

    dd

---
+++ Project/02. Back-end/김훈종/1110오전/userTB.java
@@ -0,0 +1,36 @@
+package com.soon.slt.entity;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import lombok.Data;
+
+@Data
+@Entity
+public class userTB {
+	
+	@Id
+	private String email;
+	
+	@Column(unique = true)
+	private String username;
+	
+	private String password;
+	
+	private LocalDateTime signDate;
+	
+	@OneToMany(mappedBy = "userTB", cascade = CascadeType.REMOVE)
+	private List<boardTB> boardList;
+	
+	@OneToMany(mappedBy = "userTB", cascade = CascadeType.REMOVE)
+	private List<repleTB> repleList;
+
+}
