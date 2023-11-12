0ea747ce6f27f738bc835c09bdcf276b185a1806
김다희 <kimdh5268@naver.com>
2023-11-12 16:30:36 +0900

    게시글 파일 업로드 기능 추가

---
+++ Project/02. Back-end/김다희/2023.11.12/FileRepository.java
@@ -0,0 +1,9 @@
+package com.soon.slt.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.soon.slt.entity.fileTB;
+
+public interface FileRepository extends JpaRepository<fileTB, Long>{
+
+}
