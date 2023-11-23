19522ef692653e762bc732d18e3433c0a1aa41e9
김군순 <rlarnstns@gmail.com>
2023-11-23 10:32:55 +0900

    군순오전

---
+++ ProjectWorkspace/slt/src/main/java/com/soon/slt/repository/TbBoardRepository.java
@@ -0,0 +1,13 @@
+package com.soon.slt.repository;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.soon.slt.entity.TbBoard;
+
+public interface TbBoardRepository extends JpaRepository<TbBoard, String> {
+
+	Page<TbBoard> findAll(Pageable pageable);
+
+}
---
+++ ProjectWorkspace/slt/src/main/java/com/soon/slt/repository/TbFileRepository.java
@@ -0,0 +1,9 @@
+package com.soon.slt.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.soon.slt.entity.TbFile;
+
+public interface TbFileRepository extends JpaRepository<TbFile, String> {
+
+}
