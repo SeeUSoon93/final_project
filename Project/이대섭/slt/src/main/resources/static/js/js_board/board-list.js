6fb733353c82bc0e0c3f0255e3fc78a61cd78b11
이대섭 <dleotjq1274@gmail.com>
2023-11-29 09:30:49 +0900

    commit
    header 로그아웃, 마이페이지 추가로
    html, css, js추가
    board-list 카테고리 필터링 추가로
    html, css, js추가

--- Project/이대섭/slt/src/main/resources/static/js/js_board/board-list.js
+++ Project/이대섭/slt/src/main/resources/static/js/js_board/board-list.js
@@ -44,3 +44,24 @@ document.addEventListener('DOMContentLoaded', function () {
         tbody.innerHTML += template(i, boardsObj[i]);
     }
 });
+
+// 카테고리 버튼 클릭시 카테고리별로 목록 보이게
+function filterPosts(category) {
+    const radios = document.querySelectorAll('.radio');
+    radios.forEach(radio => {
+        radio.classList.remove('active');
+    });
+
+    const posts = document.querySelectorAll('.post-item');
+    posts.forEach(post => {
+        const postCategory = post.getAttribute('data-category');
+        if (category === 'all' || postCategory === category) {
+            post.style.display = 'table-row';
+        } else {
+            post.style.display = 'none';
+        }
+    });
+
+    const activeRadio = document.querySelector(`.radio[name="radio"][onclick*="${category}"]`);
+    activeRadio.classList.add('active');
+}
