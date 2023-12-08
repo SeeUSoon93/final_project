ec6682efee727bef6a8ed2ca47e40e98b8393071
김다희 <kimdh5268@naver.com>
2023-12-08 15:41:59 +0900

    커밋

--- Project/김다희/slt/src/main/java/com/soon/slt/service/TbBoardService.java
+++ Project/김다희/slt/src/main/java/com/soon/slt/service/TbBoardService.java
@@ -38,12 +38,12 @@ public class TbBoardService {
    private final TbUserRepository tbUserRepository;
 
    // 검색 목록 리스트 조회
-   public Page<TbBoard> searchList(int page, String kw){
+   public Page<TbBoard> searchList(int page, String category, String kw){
       List<Sort.Order> sorts = new ArrayList<>();
       sorts.add(Sort.Order.desc("createdAt"));
 
       Pageable pageable = PageRequest.of(page, 10, Sort.by(sorts));
-      return this.tbBoardRepository.findAllByKeyword(kw, pageable);
+      return this.tbBoardRepository.findAllByKeyword(category, kw, pageable);
    }
 
    // 게시글 생성
