5ca6db4e095f4ea15c311721fe08ef6260a22d2f
김군순 <rlarnstns@gmail.com>
2023-12-11 23:12:24 +0900

    fuck!

--- ProjectWorkspace/fastAPI/main.py
+++ ProjectWorkspace/fastAPI/main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI, File, UploadFile, WebSocket
 from fastapi.responses import JSONResponse
 from typing import List
-from predict import predict_method
+from predict import predict_method, calculate_angles, calculate_pose_angles
 import numpy as np
 import cv2
 import mediapipe as mp
@@ -15,7 +15,9 @@ from collections import Counter
 import asyncio
 import catboost as cb
 from catboost import CatBoostClassifier
+import time
 # source venv/bin/activate
+# -m uvicorn mainapp --reload --host 0.0.0.0 --prrt 9091
 # uvicorn main:app --reload --host 0.0.0.0 --port 9091
 # uvicorn main:app --reload --host 0.0.0.0 --port 9090 --ssl-keyfile=./localhost-key.pem --ssl-certfile=./localhost.pem
 
@@ -30,9 +32,9 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+
 predictions = [] # 예측값을 저장할 리스트
 temporary_predictions = [] #임시
-most_common_prediction = None
 predicted_mapping = {'child' : '아이', 'down' : '쓰러지다', 'lost' : '잃어버리다', 'report' : '신고하다', 'sick' : '아프다', 'toilet' : '화장실', 'wallet' : '지갑', 'where' : '어디'}
 
 # 스트리밍 테스트
@@ -43,45 +45,6 @@ mp_pose = mp.solutions.pose
 pose = mp_pose.Pose()
 
 
-# 손가락 각도 계산 함수
-def calculate_angles(hand_landmarks, image_shape):
-    joint = np.zeros((21, 3))
-    for j, lm in enumerate(hand_landmarks.landmark):
-        joint[j] = [lm.x * image_shape[1], lm.y * image_shape[0], lm.z]
-        
-    # 벡터 계산
-    v1 = joint[[0,1,2,3,0,5,6,7,0,9,10,11,0,13,14,15,0,17,18,19],:]
-    v2 = joint[[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20],:]
-    v = v2 - v1
-    v = v / np.linalg.norm(v, axis=1)[:, np.newaxis]
-
-    # 각도 계산
-    angle = np.arccos(np.einsum('nt,nt->n', v[[0,1,2,4,5,6,8,9,10,12,13,14,16,17,18],:], v[[1,2,3,5,6,7,9,10,11,13,14,15,17,18,19],:]))
-    angle = np.degrees(angle)
-    
-    # NaN 값이 있을 경우 0으로 대체
-    angle = np.nan_to_num(angle)
-    
-    return angle, joint.flatten()
-
-
-def calculate_pose_angles(pose_landmarks, image_shape):
-    joint = np.zeros((33, 3))
-    for j, lm in enumerate(pose_landmarks.landmark):
-        joint[j] = [lm.x * image_shape[1], lm.y * image_shape[0], lm.z]
-
-    # 팔 각도 계산: 어깨(11, 12), 팔꿈치(13, 14), 손목(15, 16) 랜드마크 사용
-    v1 = joint[[11, 13, 12, 14], :]  # 어깨와 팔꿈치
-    v2 = joint[[13, 15, 14, 16], :]  # 팔꿈치와 손목
-    v = v2 - v1
-    v = v / np.linalg.norm(v, axis=1)[:, np.newaxis]
-
-    # 양팔의 각도 계산
-    arm_angles = np.arccos(np.einsum('nt,nt->n', v[[0, 2], :], v[[1, 3], :]))
-    arm_angles = np.degrees(arm_angles)
-
-    return arm_angles
-
 @app.websocket("/stream")
 async def video_stream(websocket: WebSocket):
     await websocket.accept()
@@ -89,6 +52,8 @@ async def video_stream(websocket: WebSocket):
     cb_model.load_model('cb_model.cbm')
     cap = cv2.VideoCapture(0)
 
+    start_time = time.time()
+    
     if not cap.isOpened():
         print("Cannot open camera")
         await websocket.close()
@@ -139,59 +104,31 @@ async def video_stream(websocket: WebSocket):
                 landmark_data = np.zeros(18)
                 
         data = np.concatenate((right_hand_angles, right_hand_coords, left_hand_angles, left_hand_coords, arm_angles, landmark_data))
-            
+
+        if websocket.client_state.value == 3:  # WebSocketState.CLOSED 상태
+            break
+
         data = data.reshape(1,-1)
         predicted_label = cb_model.predict(data)
         
-        print(predicted_label[0][0])
         # 예측된 라벨을 화면에 표시
         cv2.putText(img, text=str(predicted_label[0]), org=(50, 50),
                     fontFace=cv2.FONT_HERSHEY_SIMPLEX, fontScale=1, color=(0, 0,0), thickness=2)
-        
+        predictions.append(predicted_label[0][0])
         # 손 랜드마크 그리기
         if result.multi_hand_landmarks:
             for res in result.multi_hand_landmarks:
                 mp_drawing.draw_landmarks(img, res, mp_hands.HAND_CONNECTIONS)
         
-        if cv2.waitKey(30) == 49:  # 1번 키를 누르면 종료
-            break
         _, buffer = cv2.imencode('.jpg', img)
         await websocket.send_bytes(buffer.tobytes())
 
         # 프레임 속도 조절을 위한 대기
         await asyncio.sleep(0.1)
 
-
-
-
-
-
-
-
-
-
-
-
-@app.post("/upload")
-async def predict(image: UploadFile = File(...)):
-    # 이미지 파일을 읽어 NumPy 배열로 변환
-    image_data = await image.read()
-    nparr = np.frombuffer(image_data, np.uint8)
-    img = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
-    
-    # 이미지를 모델에 입력하여 예측
-    prediction = predict_method(img)
-    temporary_predictions.append(prediction)
-
-    if len(temporary_predictions) == 3:
-        most_common_prediction = Counter(temporary_predictions).most_common(1)[0][0]
-        predictions.append(most_common_prediction)
-        temporary_predictions.clear()
-        
-    else:
-        most_common_prediction = len(temporary_predictions)
-    
-    return {"message": most_common_prediction}
+        if time.time() - start_time >= 2:  # 2초가 지나면
+            print(len(predictions))  # 예측값의 수를 출력
+            break  # 루프 중단
 
 @app.get("/stop")
 def get_predictions():
