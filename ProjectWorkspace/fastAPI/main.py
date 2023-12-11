0f669d89f8bce8f394d489499a0bae760784081f
김군순 <rlarnstns@gmail.com>
2023-12-11 20:12:57 +0900

    321

--- ProjectWorkspace/fastAPI/main.py
+++ ProjectWorkspace/fastAPI/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, File, UploadFile
+from fastapi import FastAPI, File, UploadFile, WebSocket
 from fastapi.responses import JSONResponse
 from typing import List
 from predict import predict_method
@@ -13,7 +13,8 @@ from fastapi import FastAPI, WebSocket
 from starlette.websockets import WebSocketDisconnect
 from collections import Counter
 import asyncio
-
+import catboost as cb
+from catboost import CatBoostClassifier
 # source venv/bin/activate
 # uvicorn main:app --reload --host 0.0.0.0 --port 9091
 # uvicorn main:app --reload --host 0.0.0.0 --port 9090 --ssl-keyfile=./localhost-key.pem --ssl-certfile=./localhost.pem
@@ -34,9 +35,142 @@ temporary_predictions = [] #임시
 most_common_prediction = None
 predicted_mapping = {'child' : '아이', 'down' : '쓰러지다', 'lost' : '잃어버리다', 'report' : '신고하다', 'sick' : '아프다', 'toilet' : '화장실', 'wallet' : '지갑', 'where' : '어디'}
 
-@app.get("/main")
-def gomain():
-    return FileResponse('static/test.html')
+# 스트리밍 테스트
+mp_hands = mp.solutions.hands
+mp_drawing = mp.solutions.drawing_utils
+hands = mp_hands.Hands(max_num_hands=2, min_detection_confidence=0.5, min_tracking_confidence=0.5)
+mp_pose = mp.solutions.pose
+pose = mp_pose.Pose()
+
+
+# 손가락 각도 계산 함수
+def calculate_angles(hand_landmarks, image_shape):
+    joint = np.zeros((21, 3))
+    for j, lm in enumerate(hand_landmarks.landmark):
+        joint[j] = [lm.x * image_shape[1], lm.y * image_shape[0], lm.z]
+        
+    # 벡터 계산
+    v1 = joint[[0,1,2,3,0,5,6,7,0,9,10,11,0,13,14,15,0,17,18,19],:]
+    v2 = joint[[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20],:]
+    v = v2 - v1
+    v = v / np.linalg.norm(v, axis=1)[:, np.newaxis]
+
+    # 각도 계산
+    angle = np.arccos(np.einsum('nt,nt->n', v[[0,1,2,4,5,6,8,9,10,12,13,14,16,17,18],:], v[[1,2,3,5,6,7,9,10,11,13,14,15,17,18,19],:]))
+    angle = np.degrees(angle)
+    
+    # NaN 값이 있을 경우 0으로 대체
+    angle = np.nan_to_num(angle)
+    
+    return angle, joint.flatten()
+
+
+def calculate_pose_angles(pose_landmarks, image_shape):
+    joint = np.zeros((33, 3))
+    for j, lm in enumerate(pose_landmarks.landmark):
+        joint[j] = [lm.x * image_shape[1], lm.y * image_shape[0], lm.z]
+
+    # 팔 각도 계산: 어깨(11, 12), 팔꿈치(13, 14), 손목(15, 16) 랜드마크 사용
+    v1 = joint[[11, 13, 12, 14], :]  # 어깨와 팔꿈치
+    v2 = joint[[13, 15, 14, 16], :]  # 팔꿈치와 손목
+    v = v2 - v1
+    v = v / np.linalg.norm(v, axis=1)[:, np.newaxis]
+
+    # 양팔의 각도 계산
+    arm_angles = np.arccos(np.einsum('nt,nt->n', v[[0, 2], :], v[[1, 3], :]))
+    arm_angles = np.degrees(arm_angles)
+
+    return arm_angles
+
+@app.websocket("/stream")
+async def video_stream(websocket: WebSocket):
+    await websocket.accept()
+    cb_model = CatBoostClassifier()
+    cb_model.load_model('cb_model.cbm')
+    cap = cv2.VideoCapture(0)
+
+    if not cap.isOpened():
+        print("Cannot open camera")
+        await websocket.close()
+        return
+
+    while True:
+        ret, img = cap.read()
+        if not ret:
+            print("Ignoring empty camera frame.")
+            continue
+
+        # 이미지 처리
+        img = cv2.flip(img, 1)
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        result = hands.process(img)
+        pose_result = pose.process(img)
+        img = cv2.cvtColor(img, cv2.COLOR_RGB2BGR)
+
+        arm_angles = np.zeros(2)
+        landmark_data = np.zeros(18)
+        right_hand_angles = np.zeros(15)
+        right_hand_coords = np.zeros(63)
+        left_hand_angles = np.zeros(15)
+        left_hand_coords = np.zeros(63)   
+        
+        if result.multi_hand_landmarks:
+            for i, hand_landmarks in enumerate(result.multi_hand_landmarks):
+                if i>= 2:
+                    break
+                
+                hand_type = result.multi_handedness[i].classification[0].label
+                angles, joint_coords = calculate_angles(hand_landmarks, img.shape)
+
+                if hand_type == 'Right':
+                    right_hand_angles = angles
+                    right_hand_coords = joint_coords
+                elif hand_type == 'Left':
+                    left_hand_angles = angles
+                    left_hand_coords = joint_coords   
+        
+        if pose_result.pose_landmarks:
+            arm_angles = calculate_pose_angles(pose_result.pose_landmarks, img.shape)
+            landmark_data = []
+            for idx in [11, 13, 15, 12, 14, 16]:
+                landmark = pose_result.pose_landmarks.landmark[idx]
+                landmark_data.extend([landmark.x, landmark.y, landmark.z])
+            if len(landmark_data) == 0:
+                landmark_data = np.zeros(18)
+                
+        data = np.concatenate((right_hand_angles, right_hand_coords, left_hand_angles, left_hand_coords, arm_angles, landmark_data))
+            
+        data = data.reshape(1,-1)
+        predicted_label = cb_model.predict(data)
+        
+        print(predicted_label[0][0])
+        # 예측된 라벨을 화면에 표시
+        cv2.putText(img, text=str(predicted_label[0]), org=(50, 50),
+                    fontFace=cv2.FONT_HERSHEY_SIMPLEX, fontScale=1, color=(0, 0,0), thickness=2)
+        
+        # 손 랜드마크 그리기
+        if result.multi_hand_landmarks:
+            for res in result.multi_hand_landmarks:
+                mp_drawing.draw_landmarks(img, res, mp_hands.HAND_CONNECTIONS)
+        
+        if cv2.waitKey(30) == 49:  # 1번 키를 누르면 종료
+            break
+        _, buffer = cv2.imencode('.jpg', img)
+        await websocket.send_bytes(buffer.tobytes())
+
+        # 프레임 속도 조절을 위한 대기
+        await asyncio.sleep(0.1)
+
+
+
+
+
+
+
+
+
+
+
 
 @app.post("/upload")
 async def predict(image: UploadFile = File(...)):
@@ -66,7 +200,7 @@ def get_predictions():
     for word in predictions:
         predicted_text.append(predicted_mapping.get(word))
 
-    api_key = "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"
+    api_key = "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"
     openai.api_key = api_key
 
     input_text = ", ".join(predicted_text)
@@ -82,4 +216,4 @@ def get_predictions():
 
     predictions.clear() # 다음 요청을 위해 리스트 초기화    
     
-    return JSONResponse(content={"text": input_text})
+    return JSONResponse(content={"text": response})
