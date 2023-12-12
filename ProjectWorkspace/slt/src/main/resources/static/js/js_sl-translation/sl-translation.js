0cadc04e23bb00fe646e22edf13390ee5606f396
김다희 <kimdh5268@naver.com>
2023-12-12 11:26:58 +0900

    커밋이요
    수어번역 등등

--- ProjectWorkspace/slt/src/main/resources/static/js/js_sl-translation/sl-translation.js
+++ ProjectWorkspace/slt/src/main/resources/static/js/js_sl-translation/sl-translation.js
@@ -8,50 +8,53 @@ let socket;
 let streaming = false;
 
 function filterPosts(selId) {
-  var videoBox = document.getElementsByClassName('video-box');
-  for (var i = 0; i < videoBox.length; i++) {
-    videoBox[i].style.display = 'none';
-  }
-  var selElement = document.getElementById(selId);
-  if (selElement) {
-    selElement.style.display = 'block';
-  }
+	var videoBox = document.getElementsByClassName('video-box');
+	for (var i = 0; i < videoBox.length; i++) {
+		videoBox[i].style.display = 'none';
+	}
+	var selElement = document.getElementById(selId);
+	if (selElement) {
+		selElement.style.display = 'block';
+	}
 }
 
-window.onload = function () {
-  filterPosts('predict-video');
+window.onload = function() {
+	filterPosts('predict-video');
 }
 
 // 녹화 시작 버튼 이벤트
 document.getElementById('startRecord').addEventListener('click', () => {
-  if (!streaming) {
-    socket = new WebSocket("ws://localhost:9091/stream");
-    socket.onmessage = function (e) {
-      var arrayBuffer = e.data;
-      var blob = new Blob([arrayBuffer], { type: "image/jpeg" });
-      var img = new Image();
-      img.onload = function () {
-        webcamContext.drawImage(img, 0, 0, webcamCanvas.width, webcamCanvas.height);
-      }
-      img.src = URL.createObjectURL(blob);
-    };
-    streaming = true;
-  }
+	if (!streaming) {
+		socket = new WebSocket("ws://localhost:9091/stream");
+		socket.onmessage = function(e) {
+			var arrayBuffer = e.data;
+			var blob = new Blob([arrayBuffer], { type: "image/jpeg" });
+			var img = new Image();
+			img.onload = function() {
+				webcamContext.drawImage(img, 0, 0, webcamCanvas.width, webcamCanvas.height);
+			}
+			img.src = URL.createObjectURL(blob);
+		};
+		streaming = true;
+	}
 });
 
 document.getElementById('stopRecord').addEventListener('click', async () => {
-  if (streaming) {
-    socket.close();
-    streaming = false;
-    document.getElementById('recordingStatus').style.display = 'none';
-  
-  try {
-    const response = await fetch('http://localhost:9091/stop');
-    const data = await response.json();
-    console.log(data); // 콘솔에 응답 데이터 출력
-    document.getElementById('output').textContent = data.text; // 번역된 텍스트를 표시    
-  } catch (error) {
-    console.error('Error fetching the predictions:', error);
-  }
-}
+	if (streaming) {
+		socket.close();
+		streaming = false;
+		document.getElementById('recordingStatus').style.display = 'none';
+
+		try {
+			const response = await fetch('http://localhost:9091/stop');
+			const data = await response.json();
+			console.log(data); // 콘솔에 응답 데이터 출력
+			document.getElementById('output').textContent = data.text; // 번역된 텍스트를 표시
+
+			// 녹화를 중지한 후 'ps-video' 뷰로 전환합니다.
+			filterPosts('ps-video');
+		} catch (error) {
+			console.error('에러 발생:', error);
+		}
+	}
 });
