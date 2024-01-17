e2ca143d7550059ce082ec958962e78d23b8ecb4
김군순 <rlarnstns@gmail.com>
2024-01-17 22:04:22 +0900

    final

--- Project/김군순/slt/src/main/java/com/soon/slt/controller/TbBoardController.java
+++
@@ -1,8 +0,0 @@
-package com.soon.slt.controller;
-
-import org.springframework.stereotype.Controller;
-
-@Controller
-public class TbBoardController {
-
-}
--- Project/백지수/slt/src/main/java/com/soon/slt/controller/TbNoticeController.java
+++
@@ -1,5 +0,0 @@
-package com.soon.slt.controller;
-
-public class TbNoticeController {
-
-}
